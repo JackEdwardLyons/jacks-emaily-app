26c370239f8c79d8a81e7c84c7919def984be54f
JackEdwardLyons <jack.edward.lyons@gmail.com>
2019-04-22 13:23:05 -0600

    Add google passport for authentication

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const passport = require('passport')
+const googleStrategy = require('passport-google-oauth20').Strategy
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' })
-})
+passport.use(new googleStrategy())
+// Client ID : 1013521192175-7lep177rujqs4l2jfqd3n2j8j4qlirut.apps.googleusercontent.com 
+// Client Secret :  -WaNerYmpkQxnkl0qOgKsYeg 
 
-const PORT =  process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 app.listen(PORT)
