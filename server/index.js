e9b0ad597f976633eb9c25b0382f8fe4fd8b8a7f
JackEdwardLyons <jack.edward.lyons@gmail.com>
2019-04-22 19:18:04 -0600

    Finalize passport authentication with callback route
    Next steps are to store user in db.

--- server/index.js
+++ server/index.js
@@ -1,11 +1,39 @@
 const express = require('express')
 const passport = require('passport')
 const googleStrategy = require('passport-google-oauth20').Strategy
+const keys = require('./config/keys')
+
 const app = express()
 
-passport.use(new googleStrategy())
-// Client ID : 1013521192175-7lep177rujqs4l2jfqd3n2j8j4qlirut.apps.googleusercontent.com 
-// Client Secret :  -WaNerYmpkQxnkl0qOgKsYeg 
+/*
+ ** Google Passport Authentication 
+ ** ------------------------------- */
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.use(
+  new googleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+  }, (accessToken, refreshToken, profile, done) => {
+    console.log('access token: ', accessToken)
+    console.log('refresh token: ', refreshToken)
+    console.log('profile: ', profile)
+  })
+)
+
+app.get(
+  '/auth/google',
+  passport.authenticate(
+    'google', {
+      // tells google what access we want from their profile
+      scope: ['profile', 'email']
+    }
+  )
+)
+
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
